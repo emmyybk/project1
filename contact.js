d1400ddeb63e5e7a291bc4dfee0247ec7929acff
emma6164 <emmyy88181@gmail.com>
2023-10-20 15:46:57 +0100

    first commit

--- contact.js
+++
@@ -1,5 +0,0 @@
-function menubar(x) {
-    x.classList.toggle("change");
-    document.getElementById("mainallpages").classList.toggle("change");
-      document.getElementById('displayall').classList.toggle('change')
-  }
--- download.js
+++
@@ -1,5 +0,0 @@
-function menubar(x) {
-    x.classList.toggle("change");
-    document.getElementById("loader").classList.toggle("change");
-      document.getElementById('displayall').classList.toggle('change')
-  }
